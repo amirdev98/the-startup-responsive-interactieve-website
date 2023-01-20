0d87476cf575262c2d9d000a361e40f5858a4386
Amir Napoletano <amirnapoletano@gmail.com>
2023-01-20 08:47:48 +0100

    maak het responsive

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+// let interface = document.querySelector("a:nth-of-type(10)");
+
+// interface.addEventListener("mousedown", interfaceHandler); 
+// function interfaceHandler() {
+//   interface.classList.toggle("nth-of-type-active");
+//   interface.classList.toggle("interface");}
